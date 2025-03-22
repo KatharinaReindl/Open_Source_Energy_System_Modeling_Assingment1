fab6fc06abaca0d8c447e3d2ade84a6d4ab250b8
KatharinaReindl <e11814810@student.tuwien.ac.at>
2025-03-22 13:00:40 +0100

    add pvPower

---
+++ Pv_power.py
@@ -0,0 +1,21 @@
+import math
+
+#Calculation of the area (rectangular)
+def area (a,b):
+    area = a * b
+    return area
+
+#Calculation the number of moduls (size of one pv-module in m^2)
+def Numbers_of_modules (area,m):
+    numbers = math.floor(area/m)
+    return numbers
+
+#Calculation of the Pv-power (power of one module in Wp)
+def pv_power (numbers,powerModul):
+    power = powerModul * numbers
+    return power
+
+#Calculation of feed into the grid (example for one day)
+def feed(geneation,load):
+    feed = max(0, geneation - load)
+    return feed

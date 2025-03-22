5f2936b07dabfb1c213e925353e56189b80e34cd
KatharinaReindl <e11814810@student.tuwien.ac.at>
2025-03-22 16:47:32 +0100

    update ruff format .

--- Pv_power.py
+++ Pv_power.py
@@ -1,21 +1,25 @@
 import math
 
-#Calculation of the area (rectangular)
-def area (a,b):
+
+# Calculation of the area (rectangular)
+def area(a, b):
     area = a * b
     return area
 
-#Calculation the number of moduls (size of one pv-module in m^2)
-def numberModuls (area,m):
-    numbers = math.floor(area/m)
+
+# Calculation the number of moduls (size of one pv-module in m^2)
+def numberModuls(area, m):
+    numbers = math.floor(area / m)
     return numbers
 
-#Calculation of the Pv-power (power of one module in Wp)
-def pv_power (numbers,powerModul):
+
+# Calculation of the Pv-power (power of one module in Wp)
+def pv_power(numbers, powerModul):
     power = powerModul * numbers
     return power
 
-#Calculation of feed into the grid (example for one day)
-def feed(generation,load):
+
+# Calculation of feed into the grid (example for one day)
+def feed(generation, load):
     feed = max(0, generation - load)
     return feed
--- test_pv_power.py
+++ test_pv_power.py
@@ -1,15 +1,19 @@
-from Pv_power import area, numberModuls,pv_power,feed
+from Pv_power import area, numberModuls, pv_power, feed
+
 
 def test_area():
-    assert area(4,5)==20
+    assert area(4, 5) == 20
+
 
 def test_numbers():
-    assert numberModuls(20,3) == 6
-    assert numberModuls(20.5,4) == 5
+    assert numberModuls(20, 3) == 6
+    assert numberModuls(20.5, 4) == 5
+
 
 def test_pv_power():
-    assert pv_power(6,400) == 2400
+    assert pv_power(6, 400) == 2400
+
 
 def test_feed():
-    assert feed(7,10) == 0
-    assert feed(10,7) == 3        
+    assert feed(7, 10) == 0
+    assert feed(10, 7) == 3
